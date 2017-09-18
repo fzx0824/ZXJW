5b101d6e7c917cb77f9e27cd01ef71fcd9a7c5ff
fzx <1343853131@qq.com>
2017-09-18 09:18:04 +0800

    2017-09-18

--- src/main/java/zx/github/com/java/day/day_20170916/StringTest.java
+++ src/main/java/zx/github/com/java/day/day_20170916/StringTest.java
@@ -1,5 +1,9 @@
 package zx.github.com.java.day.day_20170916;
 
 public class StringTest {
-
+	
+	public static void main(String[] args) {
+		String s ="1.2.3.4";
+		System.out.println(s.lastIndexOf("."));
+	}
 }
---
+++ src/main/java/zx/github/com/java/day/day_20170918/ProxyStudy.java
@@ -0,0 +1,5 @@
+package zx.github.com.java.day.day_20170918;
+
+public class ProxyStudy {
+
+}
