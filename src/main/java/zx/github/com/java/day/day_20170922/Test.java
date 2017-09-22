f7c16ad8407fe706b3a14d02137d36b7dc325372
fzx <1343853131@qq.com>
2017-09-22 17:22:09 +0800

    layuiweb页面搭建

---
+++ src/main/java/zx/github/com/java/day/day_20170922/Test.java
@@ -0,0 +1,9 @@
+package zx.github.com.java.day.day_20170922;
+
+public class Test {
+	public static void main(String[] args) {
+		String a = "1", a1 = "1", a2 = "1";
+		boolean flag = a.equals("1") && a1.equals("1") && a2.equals("1");
+		System.out.println(flag);
+	}
+}
