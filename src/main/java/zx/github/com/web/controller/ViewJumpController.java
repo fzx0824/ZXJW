a8763cf6f3b8dc8247dbaa22361ffa6cc986f9be
fzx <1343853131@qq.com>
2018-01-15 16:52:47 +0800

    整合vue.

--- src/main/java/zx/github/com/web/controller/ViewJumpController.java
+++ src/main/java/zx/github/com/web/controller/ViewJumpController.java
@@ -1,5 +1,16 @@
 package zx.github.com.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/index")
 public class ViewJumpController {
 
+	@RequestMapping(value="")
+	public ModelAndView indexView2() {
+		ModelAndView mav = new ModelAndView("pages/index/index.html");
+		return mav;
+	}
 }
